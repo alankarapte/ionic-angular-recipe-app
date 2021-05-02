e649ca777d550fc8188cfbf3ba9ed3bbfb624496
alankarapte <alankar@vedirasolutions.com>
2021-05-02 22:01:09 +0530

    added recipe details screen, used paramMap, back button

--- src/app/recipes/recipe-details/recipe-details.page.ts
+++ src/app/recipes/recipe-details/recipe-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from 'src/app/models/recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-details',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-details.page.scss'],
 })
 export class RecipeDetailsPage implements OnInit {
+  recipe: Recipe;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('recipeId')) {
+        return;
+      }
+      this.recipe = this.recipeService.getRecipe(paramMap.get('recipeId'));
+    });
   }
-
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,28 +7,28 @@ import { Recipe } from '../models/recipe.model';
 export class RecipeService {
   private recipes: Recipe[] = [
     {
-      id: '01',
+      id: '1',
       imageUrl:
         'https://image.freepik.com/free-vector/good-food-logo-template_79169-17.jpg',
       ingredients: ['varan', 'bhat', 'meeth'],
       title: 'Varan bhat',
     },
     {
-      id: '02',
+      id: '2',
       imageUrl:
         'https://image.freepik.com/free-vector/good-food-logo-template_79169-17.jpg',
       ingredients: ['bhendi', 'bhaji'],
       title: 'Bhendichi bhaji',
     },
     {
-      id: '03',
+      id: '3',
       imageUrl:
         'https://image.freepik.com/free-vector/good-food-logo-template_79169-17.jpg',
       ingredients: ['chicken', 'bhat', 'rassa'],
       title: 'Chicken Biryani',
     },
     {
-      id: '04',
+      id: '4',
       imageUrl:
         'https://image.freepik.com/free-vector/good-food-logo-template_79169-17.jpg',
       ingredients: ['milk', 'shake', 'glass'],
