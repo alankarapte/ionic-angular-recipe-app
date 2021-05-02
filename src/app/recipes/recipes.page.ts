8f01a96f589e68fc07be43ebcf3ac7ae27086d64
alankarapte <alankar@vedirasolutions.com>
2021-05-02 16:49:13 +0530

    displayed recipe list on UI

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,6 @@
+/* eslint-disable max-len */
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: '01',
+      imageUrl:
+        'https://image.freepik.com/free-vector/good-food-logo-template_79169-17.jpg',
+      ingredients: ['varan', 'bhat', 'meeth'],
+      title: 'Varan bhat',
+    },
+    {
+      id: '02',
+      imageUrl:
+        'https://image.freepik.com/free-vector/good-food-logo-template_79169-17.jpg',
+      ingredients: ['bhendi', 'bhaji'],
+      title: 'Bhendichi bhaji',
+    },
+    {
+      id: '03',
+      imageUrl:
+        'https://image.freepik.com/free-vector/good-food-logo-template_79169-17.jpg',
+      ingredients: ['chicken', 'bhat', 'rassa'],
+      title: 'Chicken Biryani',
+    },
+    {
+      id: '04',
+      imageUrl:
+        'https://image.freepik.com/free-vector/good-food-logo-template_79169-17.jpg',
+      ingredients: ['milk', 'shake', 'glass'],
+      title: 'Milk Shake',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
